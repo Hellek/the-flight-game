3b87087f5426eb98231d38eb192fcc940f92b3ba
Roman Basharin <roman.basharin@quantori.com>
2025-05-11 11:03:50 +0300

    Tune OrbitControls

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,31 +1,16 @@
 import React from 'react'
-import * as THREE from 'three'
-
-import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { GlobeSphere } from './GlobeSphere'
 import { InfoPanel } from './InfoPanel'
+import { GameControls } from './GameControls'
 
 export const Game: React.FC = () => {
   return (
     <div className="grow w-full relative">
-      <Canvas
-        camera={{
-          position: [0, 0, 3],
-          fov: 45,
-        }}
-      >
+      <Canvas>
         <ambientLight intensity={3} />
         <GlobeSphere />
-        <OrbitControls
-          enableDamping
-          dampingFactor={0.05}
-          minDistance={1.1}
-          maxDistance={3}
-          rotateSpeed={0.5}
-          target={new THREE.Vector3(0, 0, 0)}
-          makeDefault
-        />
+        <GameControls />
       </Canvas>
       <InfoPanel />
     </div>
---
+++ src/components/GameControls.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { OrbitControls } from '@react-three/drei'
+
+// https://threejs.org/docs/#examples/en/controls/OrbitControls
+export const GameControls: React.FC = () => {
+  return (
+    <OrbitControls
+      enableDamping={true} // инерция вращения
+      dampingFactor={0.05} // коэффициент инерции вращения
+      minDistance={1.16}
+      enablePan={false} // запрещаем смещать, только вращать
+      maxDistance={1.8}
+      rotateSpeed={0.2}
+      makeDefault
+    />
+  )
+}
