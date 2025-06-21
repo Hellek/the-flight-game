41e5d0d00240a019e8c6065c5030aa55fb2e5352
Roman Basharin <roman.basharin@quantori.com>
2025-06-21 20:45:54 +0300

    Add Airplane 3d model

---
+++ src/components/Airplane.tsx
@@ -0,0 +1,28 @@
+import { NoseCone } from './airplane/NoseCone'
+import { Fuselage } from './airplane/Fuselage'
+import { Wings } from './airplane/Wings'
+import { TailSection } from './airplane/TailSection'
+import { type ThreeEvent } from '@react-three/fiber'
+import { type Mesh } from 'three'
+import { forwardRef } from 'react'
+
+interface AirplaneProps {
+  position?: [number, number, number]
+  rotation?: [number, number, number]
+  scale?: number
+  color?: string
+  onClick?: (e: ThreeEvent<MouseEvent>) => void
+  onPointerOver?: (e: ThreeEvent<PointerEvent>) => void
+  onPointerOut?: (e: ThreeEvent<PointerEvent>) => void
+}
+
+export const Airplane = forwardRef<Mesh, AirplaneProps>(({ color, scale = 1, rotation, ...props }, ref) => {
+  return (
+    <mesh ref={ref} scale={[scale, scale, scale]} rotation={rotation} {...props}>
+      <NoseCone color={color} />
+      <Fuselage color={color} />
+      <Wings color={color} />
+      <TailSection color={color} />
+    </mesh>
+  )
+})
---
+++ src/components/airplane/TailSection.tsx
@@ -0,0 +1,57 @@
+import { Shape, ExtrudeGeometry } from 'three'
+
+interface TailSectionProps {
+  color?: string
+}
+
+// Создаем профиль горизонтального стабилизатора
+const createHorizontalStabilizerShape = () => {
+  const shape = new Shape()
+
+  shape.moveTo(0, 0)
+  shape.bezierCurveTo(0.3, 0.1, 0.6, 0.1, 0.8, 0)
+  shape.bezierCurveTo(0.6, -0.03, 0.3, -0.03, 0, 0)
+
+  return shape
+}
+
+const horizontalStabilizerSettings = {
+  steps: 1,
+  depth: 2,
+  bevelEnabled: true,
+  bevelThickness: 0.02,
+  bevelSize: 0.02,
+  bevelSegments: 3
+}
+
+const horizontalStabilizerGeometry = new ExtrudeGeometry(
+  createHorizontalStabilizerShape(),
+  horizontalStabilizerSettings
+)
+
+export function TailSection({ color }: TailSectionProps) {
+  return (
+    <group position={[0, 0, -3.5]}>
+      {/* Горизонтальные стабилизаторы */}
+      <group position={[0, 0, 0]}>
+        {/* Левый стабилизатор */}
+        <mesh
+          position={[0, 0, 0.7]}
+          rotation={[Math.PI / 2, Math.PI / 2, Math.PI * 0.5]}
+        >
+          <primitive object={horizontalStabilizerGeometry} />
+          {color && <meshBasicMaterial color={color} />}
+        </mesh>
+
+        {/* Правый стабилизатор */}
+        <mesh
+          position={[-2.0, 0, 0.7]}
+          rotation={[Math.PI / 2, Math.PI / 2, Math.PI * 0.5]}
+        >
+          <primitive object={horizontalStabilizerGeometry} />
+          {color && <meshBasicMaterial color={color} />}
+        </mesh>
+      </group>
+    </group>
+  )
+}
